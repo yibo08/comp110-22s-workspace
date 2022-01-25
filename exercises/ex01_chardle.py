56fb07154f19d8f5078b55bb33aa578df822055f
Yibo Wang <ywang7@unc.edu>
2022-01-24 19:45:40 -0500

    Submit

---
+++ exercises/ex01_chardle.py
@@ -0,0 +1,43 @@
+"""EX01 - Chardle - A cute step toward Wordle."""
+__author__ = "730526118"
+
+
+word: str = input("Enter a 5-character word:")
+if len(word) != 5:
+    print("Error: Word must contain 5 characters")
+    exit()
+
+
+char: str = input("Enter a single character:")
+if len(char) != 1:
+    print("Error: Character must be a single character.")
+    exit()
+
+print("Searching for " + char + " in " + word)
+
+inst: int = 0
+
+if char == word[0]:
+    print(char + " found at index 0")
+    inst = inst + 1
+
+if char == word[1]:
+    print(char + " found at index 1")
+    inst = inst + 1
+
+if char == word[2]:
+    print(char + " found at index 2")
+    inst = inst + 1
+
+if char == word[3]:
+    print(char + " found at index 3")    
+    inst = inst + 1
+
+if char == word[4]:
+    print(char + " found at index 4")
+    inst = inst + 1
+
+if inst > 0:
+    print(str(inst) + " instances for " + char + " found in " + word)
+else:
+    print("No instances of " + char + " found in " + word)
